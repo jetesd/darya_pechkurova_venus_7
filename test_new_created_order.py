c9a85a4bb6ff8dc8ac71b56a7cda8dabbdcced17
d <d@MacBook-d.local>
2023-08-14 02:04:57 +0300

    fixes

--- test_new_created_order.py
+++ test_new_created_order.py
@@ -8,9 +8,8 @@ def get_user_body(first_name):
     current_body["firstName"] = first_name
     return current_body
 def test_created_order_exist():
-    user_body = get_user_body("Aaaaa")
+    user_body = get_user_body("Darya")
     create_order_response = create_order_request.post_new_order(user_body)
     track = create_order_response.json()["track"]
-    print(track)
     order_get_response = order_data_request.get_order(track)
     assert order_get_response.status_code == 200
