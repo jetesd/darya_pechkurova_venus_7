68717a6ceb31237cb47824a0b43f64369afb4b3c
d <d@MacBook-d.local>
2023-08-14 02:07:11 +0300

    fixes

--- test_new_created_order.py
+++ test_new_created_order.py
@@ -7,7 +7,7 @@ def get_user_body(first_name):
     current_body = data.order_body.copy()
     current_body["firstName"] = first_name
     return current_body
-def test_created_order_exist():
+def test_create_new_order_and_get_it_by_track_number():
     user_body = get_user_body("Darya")
     create_order_response = create_order_request.post_new_order(user_body)
     track = create_order_response.json()["track"]
